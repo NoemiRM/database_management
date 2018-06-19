dd395be33ebb2da38c2de9e7ba536de98460a543
Pilar <pilar.romerom07@gmail.com>
2018-06-19 03:37:12 -0500

    update database parameters

---
+++ features/support/database.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "mysql2",
+    :host => "localhost",
+    :database => "bookstore",
+    :username => "",
+    :password => ""
+
+)
